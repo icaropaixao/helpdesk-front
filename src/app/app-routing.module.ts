05f5637f435ef0a28fd8fca956642959c6d39ed1
Ícaro Reis <icaroreis1982@gmail.com>
2025-06-03 16:32:15 -0300

    rota para componente NAV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+
+  {
+    path:'', component: NavComponent // quando a URL estiver vazia renderiza o NavComponent 
+
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
