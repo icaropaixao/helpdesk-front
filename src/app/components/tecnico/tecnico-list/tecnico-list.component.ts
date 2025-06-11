ec401cc0ae321ddbde58f24ed881f003af2953ab
Ícaro Reis <icaroreis1982@gmail.com>
2025-06-10 21:09:34 -0300

    table para tecnicos implementado

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  ELEMENT_DATA:tecnico[] = [
+    {
+      id:1,
+    nome: 'Ícaro Reis',
+    cpf: '188.774.297-20',
+    email:'icaro@email.com',
+    senha: '123',
+    perfis:['0'],
+    dataCriacao: '10/06/2025'
+    }
+    
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email','acoes'];
+  dataSource = new MatTableDataSource<tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
+ 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
