74d1e90fc8b4e7126f4a60e944d941f56a4f9852
Ícaro Reis <icaroreis1982@gmail.com>
2025-06-23 18:40:27 -0300

    Rota para componente 'tecnico-create'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { NavComponent } from './components/nav/nav.component';
 //import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 //import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
@@ -24,7 +25,7 @@ const routes: Routes = [
       { path: 'home', component: HomeComponent },
 
       { path: 'tecnicos',            component:   TecnicoListComponent },
-      //{ path: 'tecnicos/create',     component: TecnicoCreateComponent },
+      { path: 'tecnicos/create',     component: TecnicoCreateComponent },
       //{ path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
      // { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
 
@@ -39,6 +40,8 @@ const routes: Routes = [
       //{ path: 'chamados/read/:id',              component:     ChamadoReadComponent },
     ]
   }
+
+
 ];
 
 @NgModule({
