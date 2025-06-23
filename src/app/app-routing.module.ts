b03fb6bf26ae22ecbcac76a14430db60896c8f1e
Ícaro Reis <icaroreis1982@gmail.com>
2025-06-23 20:12:26 -0300

    tecnico-update criado e implementado ao sistema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ import { NavComponent } from './components/nav/nav.component';
 //import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 //import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
@@ -24,9 +25,9 @@ const routes: Routes = [
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
 
-      { path: 'tecnicos',            component:   TecnicoListComponent },
+      { path: 'tecnicos',            component: TecnicoListComponent },
       { path: 'tecnicos/create',     component: TecnicoCreateComponent },
-      //{ path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
+      { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
      // { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
 
      // { path: 'clientes',            component:   ClienteListComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 
 // antes de iniciar o NG serve rodar o comando abaixo no bash
@@ -51,7 +52,8 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
     HeaderComponent,
     TecnicoListComponent,
     LoginComponent,
-    TecnicoCreateComponent
+    TecnicoCreateComponent,
+    TecnicoUpdateComponent
   ],
   imports: [
 
